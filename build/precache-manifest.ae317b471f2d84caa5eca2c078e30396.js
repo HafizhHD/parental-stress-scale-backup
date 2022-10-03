3618242891b4a6a02a19711379d3a9bad83f6e50
hafizhdary <hafizh.dary@defghi.id>
2022-10-03 14:05:26 +0700

    init

---
+++ build/precache-manifest.ae317b471f2d84caa5eca2c078e30396.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "53f3a655de8076d8e107",
+    "url": "/static/css/main.9960f4fd.chunk.css"
+  },
+  {
+    "revision": "53f3a655de8076d8e107",
+    "url": "/static/js/main.53f3a655.chunk.js"
+  },
+  {
+    "revision": "32f5ea9c1cdbc9b20f77",
+    "url": "/static/js/1.32f5ea9c.chunk.js"
+  },
+  {
+    "revision": "e7208c4e5e90d90adf4d",
+    "url": "/static/js/2.e7208c4e.chunk.js"
+  },
+  {
+    "revision": "54aac32d2b2a7dea149f",
+    "url": "/static/js/3.54aac32d.chunk.js"
+  },
+  {
+    "revision": "e6ffc772648489d3f73e",
+    "url": "/static/js/4.e6ffc772.chunk.js"
+  },
+  {
+    "revision": "3bcd1bbac86daace08f6",
+    "url": "/static/js/5.3bcd1bba.chunk.js"
+  },
+  {
+    "revision": "bfe85beb176afdebb15e",
+    "url": "/static/js/runtime~main.bfe85beb.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "e2c85f6dd93feb6414676aa641972d5b",
+    "url": "/index.html"
+  }
+];
